113984e611790a042efb390e3aa217373a8914ac
Sviat Wellnine <wellninedotnet@gmail.com>
2019-08-17 17:28:06 +0300

    - refactored launcher.py to aboid using Threads Threads didn't work fine because the Daemon threads are abruptly stopped at shutdown. Our program is simple so main had exited faster than Daemon started our program. So decided to use subprocess only. I didn't want to add sleep hack - compiled main and launcher to executable files

--- launcher.py
+++ launcher.py
@@ -1,32 +1,47 @@
-import threading
+import os
 import subprocess
+import time
 
-def executeFile(file_path):
-    subprocess.call(file_path, shell=True)
+
+def manageprocs(proclist):
+    """Check a list of subprocesses for processes that have
+       ended and remove them from the list.
+
+    :param proclist: list of Popen objects
+    """
+    for pr in proclist:
+        if pr.poll() is not None:
+            proclist.remove(pr)
+    # since manageprocs is called from a loop,
+    # keep CPU usage down.
+    time.sleep(0.5)
 
 
 def main():
-    file = None
 
+    # Read config file
     try:
-        file = open('./config.ini', 'r');
-    except:
-        # TODO: add alert widget
-        print("cant find a file")
+        with open('./config.ini', 'r') as f:
+            pathes = [path.strip() for path in f.readlines()]
+    except FileNotFoundError:
+        print("cant find config file")
+        exit(1)
 
-    pathes = [ path.strip() for path in file.readlines() ]
-
-    try:
-        for idx in range(len(pathes)):
-            print(pathes[idx])
-            file_path = pathes[idx];
-            newThread = threading.Thread(target=executeFile, args=(file_path,))
-            newThread.daemon = True
-            newThread.start()
-    except:
-        print("cant start thread")
+    # List of subprocesses
+    procs = []
+    # Do not launch more processes concurrently than your
+    # CPU has cores.  That will only lead to the processes
+    # fighting over CPU resources.
+    maxprocs = os.cpu_count()
+    # Launch all subprocesses.
+    for path in pathes:
+        while len(procs) == maxprocs:
+            manageprocs(procs)
+        procs.append(subprocess.Popen(path, shell=True))
+    # Wait for all subprocesses to finish.
+    while len(procs) > 0:
+        manageprocs(procs)
 
 
 if __name__ == '__main__':
     main()
-    input("Press enter to exit ;)")
